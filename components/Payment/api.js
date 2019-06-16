c8652f710de137d98a8bb8d44523d333af20f7f9
Ania-chan <ania.jurago@gmail.com>
2019-06-16 17:59:39 +0900

    added input field

--- components/Payment/api.js
+++ components/Payment/api.js
@@ -9,7 +9,7 @@ export const doPayment = (amount, tokenId, accessToken) => {
     'Content-Type': 'application/json',
   };
   return axios
-    .post('http://localhost:5000/api/doPayment', body, { headers })
+    .post('https://proud-stories-staging.herokuapp.com/api/doPayment', body, { headers })
     .then(({ data }) => {
       return data;
     })
--- components/Payment/scenes/CardFormScreen.js
+++ components/Payment/scenes/CardFormScreen.js
@@ -3,11 +3,14 @@ import { View, Text, StyleSheet } from 'react-native'
 import stripe from 'tipsi-stripe'
 import Button from '../components/Button'
 import testID from '../utils/testID'
+import { doPayment } from '../api'
+import { Item, Input } from 'native-base';
 
 export default class CardFormScreen extends PureComponent {
   static title = 'Card Form'
 
   state = {
+    amount: 0,
     loading: false,
     token: null,
   }
@@ -15,25 +18,10 @@ export default class CardFormScreen extends PureComponent {
   handleCardPayPress = async () => {
     try {
       this.setState({ loading: true, token: null })
-      const token = await stripe.paymentRequestWithCardForm({
-        // Only iOS support this options
-        smsAutofillDisabled: true,
-        requiredBillingAddressFields: 'full',
-        prefilledInformation: {
-          billingAddress: {
-            name: 'Gunilla Haugeh',
-            line1: 'Canary Place',
-            line2: '3',
-            city: 'Macon',
-            state: 'Georgia',
-            country: 'US',
-            postalCode: '31217',
-            email: 'ghaugeh0@printfriendly.com',
-          },
-        },
-      })
-
-      this.setState({ loading: false, token })
+      const token = await stripe.paymentRequestWithCardForm()
+      this.setState({ token })
+      const payment = await doPayment(this.state.amount, this.state.token.tokenId)
+      this.setState({ loading: false })
     } catch (error) {
       this.setState({ loading: false })
     }
@@ -47,6 +35,9 @@ export default class CardFormScreen extends PureComponent {
         <Text style={styles.header}>
           Pay by Credit Card
         </Text>
+        <Item rounded style={styles.input}>
+            <Input placeholder="How much credits to charge" />
+          </Item>
         <Text style={styles.instruction}>
           Click button to show Card Form dialog.
         </Text>
@@ -89,4 +80,8 @@ const styles = StyleSheet.create({
   token: {
     height: 20,
   },
+  input: {
+    backgroundColor: "white",
+    width: 270
+  }
 })
