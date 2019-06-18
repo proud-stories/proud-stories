4eb8486de5c41ec45f459b562f2246fc965e678a
Konstantin Schlegel <konst2011@hotmail.de>
2019-06-18 03:47:20 +0200

    Did some things idk

--- components/HomeTab/Home.js
+++ components/HomeTab/Home.js
@@ -10,7 +10,7 @@ import {
 
 import { Container, Content, Icon, Card, CardItem, Body } from 'native-base'
 import CardComponent from '../CardComponent'
-import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
+import { RecyclerListView, DataProvider, LayoutProvider } from 'recyclerlistview';
 import Modal from "react-native-modal";
 import MultiSelect from 'react-native-multiple-select'
 
@@ -18,43 +18,15 @@ class HomeTab extends Component {
     constructor(args) {
         super(args);
         this.state = {
-            videos: [
-                {
-                    title: "Uganda Pineapples",
-                    description: "Pineapples of Uganda are delicious.",
-                    url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/waits.mp4",
-                    likes: 100,
-                    paused: true,
-                    id: 1
-                },
-                {
-                    title: "Pineapple wine!",
-                    description: "Did you know you can make wine from pineapple? It's so delicious.",
-                    url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/waits.mp4",
-                    likes: 251,
-                    paused: true,
-                    id:2
-                },
-                {
-                    title: "Uganda Pineapples Peeling",
-                    description: "This is how to peel a pineapple.",
-                    url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/video.mp4",
-                    likes: 298,
-                    paused: true,
-                    id:3
-                },
-                {
-                    title: "Uganda Pineapples Peeling",
-                    description: "This is how to peel a pineapple.",
-                    url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/waits.mp4",
-                    likes: 298,
-                    paused: true,
-                    id: 4
-                }
-            ]
+            videos: [],
+            visibleModal: "",
+            selectedItems: [],
+            categories: [],
+            multiSelect: ""
+
         }
         this.state.dataProvider = new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(this.state.videos)
-        
+
         this._layoutProvider = new LayoutProvider(_index => 'card', (_type, dim) => {
             dim.width = Dimensions.get('window').width;
             dim.height = 420;
@@ -66,27 +38,14 @@ class HomeTab extends Component {
     _renderRow(_type, data, index) {
         return <CardComponent
             className={'card'}
+            id={data.id}
             title={data.title}
             description={data.description}
             url={data.url}
-            likes={data.likes}
-            style={{margin:0}}
-            key={Math.round(Math.random()*10000000)}
+            likes={data.count}
+            style={{ margin: 0 }}
+            key={Math.round(Math.random() * 10000000)}
         />
-              
-    state = {
-        videos: [],
-        visibleModal: "",
-        selectedItems: [],
-        categories: [],
-        multiSelect: ""
-
-    }
-
-    static navigationOptions = {
-        tabBarIcon: ({ tintColor }) => (
-            <Icon name="ios-home" style={{ color: tintColor }} />
-        )
     }
 
     handleScroll = (e) => {
@@ -115,9 +74,11 @@ class HomeTab extends Component {
                     item.paused = false;
                     this.setState({ videos: [item, ...this.state.videos] })
                 })
+                console.log(this.state.videos)
                 //update the dataProvider
-                this.setState({ dataProvider:
-                    new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(this.state.videos)
+                this.setState({
+                    dataProvider:
+                        new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(this.state.videos)
                 })
             })
             .catch((err) => { throw Error(err) });
@@ -131,15 +92,13 @@ class HomeTab extends Component {
         const { selectedItems } = this.state;
         return (
             <Container style={styles.container}>
-                <Content>
-                    <RecyclerListView
-                        style={{ height: Dimensions.get('window').height*.8, width: Dimensions.get('window').width }}
-                        rowRenderer={this._renderRow}
-                        dataProvider={this.state.dataProvider}
-                        layoutProvider={this._layoutProvider}
-                    />
-            <Container  >
                 <Button onPress={() => this.setState({ visibleModal: "fancy" })} title="Filter" />
+                <RecyclerListView
+                    style={{ height: Dimensions.get('window').height * .8, width: Dimensions.get('window').width }}
+                    rowRenderer={this._renderRow}
+                    dataProvider={this.state.dataProvider}
+                    layoutProvider={this._layoutProvider}
+                />
                 <Content onScroll={this.handleScroll} scrollEventThrottle={16}>
                     <View>
                         {this.state.videos.map((video) => (
