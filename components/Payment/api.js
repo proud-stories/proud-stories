aa566012a7880100f15f955336bd81abcca5317f
Ben Dyer <dyer.ben@protonmail.com>
2019-06-24 21:52:30 +0900

    Add APP_URL env variable

--- components/Payment/api.js
+++ components/Payment/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Config from "react-native-config";
+import Config from 'react-native-config'
 
 export const doPayment = (amount, tokenId, accessToken) => {
   const body = {
